672e8a5829eb0e4b74aed42b302f7af9d251facc
kuangch <kuangch_8017@qq.com>
2018-11-23 18:09:25 +0800

    test 1

--- data/device.js
+++ data/device.js
@@ -5,7 +5,8 @@ let {Mock} = require(require('app-root-path') + '/utils/data_utils')
 
 let device_info = {
     'device_id': '@guid',
-    'last_login':'@now'
+    'last_login':'@now',
+    'test':1
 };
 
 module.exports = {
